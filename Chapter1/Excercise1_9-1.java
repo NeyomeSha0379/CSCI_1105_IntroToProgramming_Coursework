3393821d781cdacc2bf4dce371c13d5196b86281
NeyomeSha0379 <50683413+NeyomeSha0379@users.noreply.github.com>
2019-05-16 12:22:19 -0600

    Add files via upload

---
+++ Chapter1/Excercise1_9-1.java
@@ -0,0 +1,15 @@
+/*
+ * Shane Neyome
+ * 05/15/19
+ */
+public class Excercise1_9 {
+
+	public static void main(String[] args) {
+	System.out.print("area =");
+	System.out.println(4.5 * 7.9);
+	
+	System.out.print("perimeter =");
+	System.out.println((4.5 * 2) + (7.9 * 2));
+	}
+
+}
---
+++ Chapter1/Exercise1_13.java
@@ -0,0 +1,16 @@
+
+/*
+ * Shane Neyome
+ * 05/15/19
+ */
+public class Exercise1_13 {
+
+	public static void main(String[] args) {
+		System.out.print("x = ");
+		System.out.println(((44.5 * 0.55) - (50.2 * 5.9)) / ((3.4 * 0.55) - (50.2 * 2.1)));
+
+		System.out.print("y = ");
+		System.out.println(((3.4 * 5.9) - (44.5 * 2.1)) / ((3.4 * 0.55) - (50.2 * 2.1)));
+	}
+
+}
